1a00981b7bbe0246fc8999e3e8b13434fded1281
sharonelbo <sharon4homework@gmail.com>
2021-05-08 18:18:27 +0300

    commit

--- game_cards/Card.py
+++ game_cards/Card.py
@@ -1,19 +1,20 @@
 class Card:
-    def __init__(self, suit, value):
+    def __init__(self, value, suit):
         """creates a card that has a suit and a value"""
         if type(value) == int:
-            if 0<=value<=13:
+            if 0 <= value <= 13:
                 self.value = value
         else:
             self.value = None
-            return "Error"
 
         if suit != "diamond" and suit != "spade" and suit != "heart" and suit != "club":
             self.suit = None
         else:
             self.suit = suit
 
-        if self.suit == "diamond":
+        if self.suit is None:
+            self.__power = 0
+        elif self.suit == "diamond":
             self.__power = 1
         elif self.suit == "spade":
             self.__power = 2
@@ -21,8 +22,7 @@ class Card:
             self.__power = 3
         elif self.suit == "club":
             self.__power = 4
-        elif self.suit == None:
-            self.__power = 0
+
 
     def __str__(self):
         """"""
@@ -30,26 +30,34 @@ class Card:
 
     def __eq__(self, other):
         """return True if two cards are equal and False if not"""
-        if self.value == other.value and self.__power == other.__power:
-            return True
-        else:
-            return False
-
-    def __gt__(self, other):
-        """return True if a card is greater than the other and False if not"""
-        if self.value == 1 and other.value > 1:
-            return True
-        if self.value > 1 and other.value == 1:
-            return False
-        if self.value > other.value:
-            return True
-        elif self.value < other.value:
-            return False
-        else:
-            if self.__power > other.__power:
+        if type(self.value) == int and type(self.__power == int):
+            if self.value == other.value and self.__power == other.__power:
                 return True
             else:
                 return False
+        else:
+            print("Error one card or more isnt valid")
+            return
+
+    def __gt__(self, other):
+        """return True if a card is greater than the other and False if not"""
+        if type(self.value) == int and type(self.__power == int):
+            if self.value == 1 and other.value > 1:
+                return True
+            if self.value > 1 and other.value == 1:
+                return False
+            if self.value > other.value:
+                return True
+            if self.value < other.value:
+                return False
+            else:
+                if self.__power > other.__power:
+                    return True
+                else:
+                    return False
+        else:
+            print("Error one card or more isnt valid")
+            return
 
     def __repr__(self):
         return f"{self.value} {self.suit}"
