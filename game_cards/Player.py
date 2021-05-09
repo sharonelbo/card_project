aa0ae6d097a9002892e82fb66c9683d227fe0edf
sharonelbo <sharon4homework@gmail.com>
2021-05-09 10:03:15 +0300

    commit

--- game_cards/Player.py
+++ game_cards/Player.py
@@ -1,19 +1,30 @@
 import random
-
-
+from game_cards.DeckOfCards import DeckOfCards
+from game_cards.Card import Card
 class Player:
     def __init__(self, name, num_of_cards=10):
-        self.name = name
-        if num_of_cards > 26:
-            self.num_of_cards = 26
+        if type(name) == str:
+            self.name = name
         else:
-            self.num_of_cards = num_of_cards
+            self.name = "player"
+        if type(num_of_cards) == int:
+             if num_of_cards > 26:
+                 self.num_of_cards = 26
+             else:
+                 self.num_of_cards = num_of_cards
+             if num_of_cards < 0:
+                self.num_of_cards = 0
+        else:
+            self.num_of_cards = 10
         self.__hand = []
 
     def set_hand(self, deck):
         """get a deck and give the player new cards for the hand"""
-        for i in range(self.num_of_cards):
-            self.__hand.append(deck.deal_one())
+        if type(deck) == DeckOfCards:
+            for i in range(self.num_of_cards):
+                self.__hand.append(deck.deal_one())
+        else:
+            print("Error you need to enter a valid deck")
 
     def __str__(self):
         return f"name: {self.name}, hand: {self.__hand}"
@@ -23,11 +34,17 @@ class Player:
 
     def get_card(self):
         """take a random card from the player"""
-        return self.__hand.pop(random.randint(0, len(self.__hand) - 1))
+        if len(self.__hand) > 0:
+            return self.__hand.pop(random.randint(0, len(self.__hand) - 1))
+        else:
+            print("the player doesnt have cards in his hands")
 
     def add_card(self, card):
         """gets a card and add it to the player hand"""
-        self.__hand.append(card)
+        if type(card) == Card:
+            self.__hand.append(card)
+        else:
+            print("Error you need to enter a valid card")
 
     def show(self):
         """prints the player name and hand"""
